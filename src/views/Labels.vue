650c412e827c6842cfdafc628479231c442c5997
eggs <550467116.com>
2020-10-28 22:12:54 +0800

    添加标签可跳转至编辑页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,11 @@
 <template>
 <layout>
   <ol class="tags">
-    <li v-for="tag in tags" :key="tag.id">
+    <router-link class="tag" v-for="tag in tags" :key="tag.id"
+                 :to="`/labels/edit/${tag.id}`">
       <span>{{tag.name}}</span>
       <Icon name="right"/>
-    </li>
+    </router-link>
   </ol>
   <div class="createTag-wrapper">
     <button class="createTag" @click="createTag">新建标签</button>
@@ -40,7 +41,7 @@ export default class  Labels extends Vue{
   background: white;
   font-size: 16px;
   padding-left: 16px;
-  >li{
+  > .tag{
     min-height: 44px;
     display: flex;
     align-items: center;
