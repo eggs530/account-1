3ced2578bf637c8561afed126c16b48fa95337e2
eggs <550467116@.qq.com>
2020-11-03 16:09:09 +0800

    将 create 封装到recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -13,14 +13,14 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import {Component, Watch} from "vue-property-decorator";
-import NumberPad from "@/components/Money/NumberPad.vue";
-import Tags from "@/components/Money/Tags.vue";
-import FormItem from "@/components/Money/FormItem.vue";
-import Types from "@/components/Money/Types.vue";
-import {recordListModel} from "@/models/recordListModel";
-import {tagListModel} from "@/models/tagListModel";
+import Vue from 'vue';
+import {Component, Watch} from 'vue-property-decorator';
+import NumberPad from '@/components/Money/NumberPad.vue';
+import Tags from '@/components/Money/Tags.vue';
+import FormItem from '@/components/Money/FormItem.vue';
+import Types from '@/components/Money/Types.vue';
+import {recordListModel} from '@/models/recordListModel';
+import {tagListModel} from '@/models/tagListModel';
 
 const recordList = recordListModel.fetch();
 const tagList = tagListModel.fetch();
@@ -33,7 +33,7 @@ export default class Money extends Vue {
   tags = tagList;
   recordList: RecordItem[] = recordList;
   record: RecordItem = {
-    tags: [], notes: "", type: "+", amount: 0
+    tags: [], notes: '', type: '+', amount: 0
   };
 
   onUpdateTags(value: string[]) {
@@ -45,14 +45,12 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const record2: RecordItem = recordListModel.clone(this.record);
-    record2.createdAt = new Date();
-    this.recordList.push(record2);
+    recordListModel.create(this.record);
   }
 
-  @Watch("recordList")
+  @Watch('recordList')
   onRecordListChange() {
-    recordListModel.save(this.recordList);
+    recordListModel.save();
   }
 }
 </script>
