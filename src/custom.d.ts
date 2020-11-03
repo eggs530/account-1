3f807e2283c016836963e65febd190ba2daa651b
eggs <550467116@.qq.com>
2020-11-03 16:36:52 +0800

    使用全局 tagList

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -5,3 +5,21 @@ type RecordItem={
     amount:number;
     createdAt?:Date
 }
+type Tag = {
+    id: string;
+    name: string;
+}
+type TagListModel = {
+    data: Tag[];
+    fetch: () => Tag[];
+    create: (name: string) => "success" | "duplicated";
+    //success表示成功 duplicated表示内容重复
+    //联合类型：防止字符串拼写错误 可直接列举具体的字符串表明类型
+    update: (id: string, name: string) => "success" | "not found" | "duplicated";
+    remove:(id:string)=>boolean;
+    save: () => void;//不返回
+}
+interface Window{
+    tagList:Tag[]
+}
+
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -20,17 +20,15 @@ import Tags from '@/components/Money/Tags.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Types from '@/components/Money/Types.vue';
 import {recordListModel} from '@/models/recordListModel';
-import {tagListModel} from '@/models/tagListModel';
 
 const recordList = recordListModel.fetch();
-const tagList = tagListModel.fetch();
 //声明一个类型
 
 @Component({
   components: {Types, FormItem, Tags, NumberPad}
 })
 export default class Money extends Vue {
-  tags = tagList;
+  tags = window.tagList;
   recordList: RecordItem[] = recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '+', amount: 0
