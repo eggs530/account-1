b2b84e0f7601fe2cc2dcb681b35e35d6dc8fd3d2
eggs <550467116.com>
2020-11-03 21:59:26 +0800

    将所有数据操作封装在 store 里

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -18,13 +18,5 @@ type TagListModel = {
     save: () => void;//不返回
 }
 
-interface Window {
-    tagList: Tag[];
-    createTag: (name: string) => void;
-    removeTag: (id: string) => boolean;
-    updateTag: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
-    findTag: (id: string) => Tag|undefined;
-    recordList: RecordItem[];
-    createRecord: (record: RecordItem) => void;
-}
+
 
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -22,16 +22,17 @@ import Vue from "vue";
 import {Component} from "vue-property-decorator";
 import FormItem from "@/components/Money/FormItem.vue";
 import Button from "@/components/Button.vue";
+import store from "@/store/index2";
 
 @Component({
   components: {Button, FormItem}
 })
 export default class EditLabel extends Vue {
   //tag的初始值是undefined,类型是一个有id和name的对象，可能为空？
-  tag?: Tag= undefined;
+  tag?: Tag = undefined;
 
   created() {
-    this.tag = window.findTag(this.$route.params.id);
+    this.tag = store.findTag(this.$route.params.id);
     if (!this.tag) {
       this.$router.replace("/404");//路由器
     }
@@ -39,20 +40,22 @@ export default class EditLabel extends Vue {
 
   update(name: string) {
     if (this.tag) {
-      window.updateTag(this.tag.id,name);
+      store.updateTag(this.tag.id, name);
     }
   }
+
   remove() {
     if (this.tag) {
-      if (window.removeTag(this.tag.id)) {
+      if (store.removeTag(this.tag.id)) {
         this.$router.back();
       } else {
-        window.alert('删除失败');
+        window.alert("删除失败");
       }
     }
   }
-  goBack(){
-    this.$router.back()
+
+  goBack() {
+    this.$router.back();
   }
 }
 </script>
