2bb7b7ec0565d5ee19b62bcdfed60a9db9ba0796
eggs <550467116@.qq.com>
2020-11-10 17:05:29 +0800

    完成列表的 CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,23 +2,20 @@
   <layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync='type'/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync='interval'/>
-    <div>
-      type:{{ type }}
-    </div>
-    <br/>
-    interval:{{ interval }}
-    <div>
       <ol>
         <li v-for="(group,index) in result" :key="index">
-          <h3>{{group.title}}</h3>
+          <h3 class="title">{{group.title}}</h3>
           <ol>
-            <li v-for="item in group.items" :key="item.id">
-              {{item.amount}} {{item.createdAt}}
+            <li v-for="item in group.items" :key="item.id"
+                class="record"
+            >
+             <span>{{tagString(item.tags)}}</span>
+              <span class="notes">{{item.notes}}</span>
+              <span>￥{{item.amount}}</span>
             </li>
           </ol>
         </li>
       </ol>
-    </div>
   </layout>
 </template>
 
@@ -34,13 +31,17 @@ import recordTypeList from "@/constants/recordTypeList";
   components: {Tabs}
 })
 export default class Statistics extends Vue {
+  tagString(tags:Tag[]){
+    return tags.length===0?'无':tags.join(',')
+  }
   get recordList() {
     return (this.$store.state as RootState).recordList;
   }
 
   get result() {
     const {recordList} = this;
-    type HashTableValue = { title: string; items: RecordList[] }
+    type HashTableValue = { title: string,items: RecordList[] }
+
     const hashTable: { [key: string]: HashTableValue } = {};
     for (let i = 0; i < recordList.length; i++) {
       const [date, time] = recordList[i].createdAt!.split('T');
@@ -76,6 +77,25 @@ export default class Statistics extends Vue {
 
   .interval-tabs-item {
     height: 48px;
+  }
+  %item{
+    padding:8px 16px;
+    line-height: 24px;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+  }
+  .title{
+    @extend %item
+  }
+  .record{
+    background: white;
+    @extend %item
+  }
+  .notes{
+    margin-right:auto;
+    margin-left:16px;
+    color: #999;
   }
 }
 </style>
