fa2f352997acbaafb70a928f14823b9aaa5de857
eggs <550467116.com>
2020-11-15 14:00:03 +0800

    优化PC界面样式

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
-   <div id="app">
-     <router-view/>
-   </div>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
 
 
 <style lang="scss">
 @import "~@/assets/style/reset.scss";
 @import "~@/assets/style/helper.scss";
-body{
+
+body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #333;
   line-height: 1.5;
-  font-family:$font-hei;
+  font-family: $font-hei;
   font-size: 16px;
-  background:#f5f5f5;
+  background: #f5f5f5;
+}
+
+#app {
+  max-width: 500px;
+  margin: 0 auto;
 }
 </style>
