8472161b7fa7180bbdfc4343bb1e46ed35398ee5
eggs <550467116@.qq.com>
2020-11-19 15:47:06 +0800

    添加 createdAt 输入框

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -4,6 +4,14 @@
     <Tabs :data-source="recordTypeList"
           :value.sync="record.type"
     />
+    <div class="createdAt">
+      <FormItem
+          field-name="日期"
+          type="date"
+          placeholder="在这里输入日期"
+          :value.sync="record.createdAt"
+      />
+    </div>
     <div class="notes">
       <FormItem
           field-name="备注"
@@ -36,7 +44,7 @@ export default class Money extends Vue {
   recordTypeList = recordTypeList;
 
   record: RecordItem = {
-    tags: [], notes: '', type: '+', amount: 0
+    tags: [], notes: '', type: '+', amount: 0,createdAt:new Date().toISOString()
   };
 
   created() {
